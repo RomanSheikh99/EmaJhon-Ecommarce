7c03ea287657236431f33edb486f4deb06055aef
roman <romansheikh170@gmail.com>
2021-10-11 01:10:16 +0600

    --- Auto-Git Commit ---

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDOzHWAbgXCjdQ1X3NabJzHp1VsY4pKfZs",
+  authDomain: "ema-jo.firebaseapp.com",
+  projectId: "ema-jo",
+  storageBucket: "ema-jo.appspot.com",
+  messagingSenderId: "911197933866",
+  appId: "1:911197933866:web:9179d6280ba22a656a8e53"
+};
+
+export default firebaseConfig;
