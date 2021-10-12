fce2bcbe3f9118d7b18604e48e425542c801dcd5
roman <romansheikh170@gmail.com>
2021-10-12 14:44:05 +0600

    --- Auto-Git Commit ---

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -7,15 +7,15 @@ import './register.css';
 const Register = () => {
     return (
         <div className='register'>
-            <h2>Resigter Now</h2>
+            <h2 className="text-5xl mb-4">Resigter Now</h2>
             <form>
                 <input type="text" placeholder="Your Full Name" />
                 <input type="email" placeholder="Your Email Address" />
                 <input type="password" placeholder="New Password" />
                 <input type="password" placeholder="Re-enter Password" />
-                <input type="submit" value="Submit" />
+                <input className="bg-yellow-300 cursor-pointer w-2/4 text-2xl mt-4" type="submit" value="Submit" />
             </form>
-            <p>Allready Have An Account? <Link to="/logIn">Login In</Link></p>
+            <p>Allready Have An Account? <Link className="text-yellow-600 m-10" to="/logIn">Login In</Link></p>
             <p>----------Or----------</p>
             <div>
                 <FontAwesomeIcon icon={faRecycle} />
